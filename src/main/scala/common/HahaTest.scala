d8320f4021947f7eed366c70eb1ade420e0fea50
bestchenwu <bestchenwu@163.com>
2020-11-08 10:37:10 +0800

    提交scala依赖

---
+++ src/main/scala/common/HahaTest.scala
@@ -0,0 +1,8 @@
+package common
+
+object HahaTest {
+    
+  def main(args: Array[String]): Unit = {
+      println("hello")
+  }
+}
---
+++ src/test/java/HahaTest.scala
@@ -0,0 +1,8 @@
+
+
+object HahaTest {
+    
+    def main(args: Array[String]): Unit = {
+        println("haha");
+    }
+}
